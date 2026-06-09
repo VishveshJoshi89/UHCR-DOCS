83bec8f256dac5c80578aed397c6b1fb592961ca
VishveshJoshi89 <vishveshj8989@gmail.com>
2026-06-09 14:28:01 +0530

    update of routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/UHCR-DOCS/",
 
-  base:'/',
-
-  plugins:[
+  plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // Automatically updates the service worker
+      registerType: "autoUpdate", // Automatically updates the service worker
       manifest: {
-        name: 'UHCR-DOCS',
-        short_name: 'UHCR',
-        theme_color: '#ffffff',
-        icons: [{
-            src: './public/image/logo.png',
-            sizes: '192x192',
-            type: 'image/png'
+        name: "UHCR-DOCS",
+        short_name: "UHCR",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./public/image/logo.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: './public/image/logo.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "./public/image/logo.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: './public/image/logo.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable' // Recommended for Android adaptive icons
-          }]
-      }
-    })
-  ]
-
-})
+            src: "./public/image/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable", // Recommended for Android adaptive icons
+          },
+        ],
+      },
+    }),
+  ],
+});
